53be8236461bd4f2acc47fe7f25447de195f2ad1
VASANTHAKUMAR. J <96156776+Vasanthakumar23@users.noreply.github.com>
2024-06-21 23:23:44 +0530

    lock files 3

--- src/containers/education/card.jsx
+++ src/containers/education/card.jsx
@@ -1,25 +1,24 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Fade from "react-reveal/Fade";
 import eduImgBlack from "../../assets/svg/education/eduImgBlack.svg";
 import "./index.css";
 
 function EducationCard({ institution, course, duration }) {
-  const useStyles = makeStyles((t) => ({
-    educationCard: {
-      backgroundColor: "#f5f5f5",
-      "&:hover": {
-        backgroundColor: "f8f8f8",
-        border: "1px solid grey",
-      },
-    },
-  }));
+  // const useStyles = makeStyles((t) => ({
+  //   educationCard: {
+  //     backgroundColor: "#f5f5f5",
+  //     "&:hover": {
+  //       backgroundColor: "f8f8f8",
+  //       border: "1px solid grey",
+  //     },
+  //   },
+  // }));
 
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
     <Fade bottom>
-      <div className={`education-card  ${classes.educationCard}`}>
+      <div className={`education-card  `}>
         <div className="educard-img" style={{ backgroundColor: "#ffc200" }}>
           <img src={eduImgBlack} alt="" />
         </div>
